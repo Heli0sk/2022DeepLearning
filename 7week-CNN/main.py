ea22c466c8ce097a1330ce96d8b18e7bb5bdbaa0
Jinx <13206481090@163.com>
2022-04-12 09:28:47 +0800

    Update LeNet-5

--- 7week-CNN/main.py
+++ 7week-CNN/main.py
@@ -1,43 +1,91 @@
-from model import Model
-import numpy as np
 import torch
-from torchvision.datasets import mnist
-from torch.nn import CrossEntropyLoss
-from torch.optim import SGD
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+import torch.optim as optim
 from torch.utils.data import DataLoader
-from torchvision.transforms import ToTensor
+from data import Mnist
+from model import LeNet5
 
-if __name__ == '__main__':
-    batch_size = 256
-    train_dataset = mnist.MNIST(root='./train', train=True, transform=ToTensor())
-    test_dataset = mnist.MNIST(root='./test', train=False, transform=ToTensor())
-    train_loader = DataLoader(train_dataset, batch_size=batch_size)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size)
-    model = Model()
-    sgd = SGD(model.parameters(), lr=1e-1)
-    loss_fn = CrossEntropyLoss()
-    all_epoch = 100
+# 生成训练集
+train_set = Mnist(
+    root='dataset',
+    train=True,
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1037,), (0.3081,))
+    ])
+)
+train_loader = DataLoader(
+    dataset=train_set,
+    batch_size=32,
+    shuffle=True
+)
 
-    for current_epoch in range(epoch):
-        model.train()
-        for idx, (train_x, train_label) in enumerate(train_loader):
-            sgd.zero_grad()
-            predict_y = model(train_x.float())
-            loss = loss_fn(predict_y, train_label.long())
-            if idx % 10 == 0:
-                print('idx: {}, loss: {}'.format(idx, loss.sum().item()))
-            loss.backward()
-            sgd.step()
+# 实例化一个网络
+net = LeNet5()
 
-        all_correct_num = 0
-        all_sample_num = 0
-        model.eval()
-        for idx, (test_x, test_label) in enumerate(test_loader):
-            predict_y = model(test_x.float()).detach()
-            predict_y = np.argmax(predict_y, axis=-1)
-            current_correct_num = predict_y == test_label
-            all_correct_num += np.sum(current_correct_num.numpy(), axis=-1)
-            all_sample_num += current_correct_num.shape[0]
-        acc = all_correct_num / all_sample_num
-        print('accuracy: {:.2f}'.format(acc))
-        torch.save(model, 'models/mnist_{:.2f}.pkl'.format(acc))
+# 定义损失函数和优化器
+loss_function = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(
+    net.parameters(),
+    lr=0.001,
+    momentum=0.9
+)
+
+# 3 训练模型
+loss_list = []
+for epoch in range(10):
+    running_loss = 0.0
+    for batch_idx, data in enumerate(train_loader, start=0):
+
+        images, labels = data  # 读取一个batch的数据
+        optimizer.zero_grad()  # 梯度清零，初始化
+        outputs = net(images)  # 前向传播
+        loss = loss_function(outputs, labels)  # 计算误差
+        loss.backward()  # 反向传播
+        optimizer.step()  # 权重更新
+        running_loss += loss.item()  # 误差累计
+
+        # 每300个batch 打印一次损失值
+        if batch_idx % 300 == 299:
+            print('epoch:{} batch_idx:{} loss:{}'
+                  .format(epoch + 1, batch_idx + 1, running_loss / 300))
+            loss_list.append(running_loss / 300)
+            running_loss = 0.0  # 误差清零
+
+print('Finished Training.')
+
+# 打印损失值变化曲线
+plt.plot(loss_list)
+plt.title('traning loss')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.show()
+
+# 测试
+test_set = Mnist(
+    root='dataset',
+    train=False,
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1037,), (0.3081,))
+    ])
+)
+test_loader = DataLoader(
+    dataset=test_set,
+    batch_size=32,
+    shuffle=True
+)
+
+correct = 0  # 预测正确数
+total = 0  # 总图片数
+
+for data in test_loader:
+    images, labels = data
+    outputs = net(images)
+    _, predict = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predict == labels).sum()
+
+print('测试集准确率 {}%'.format(100 * correct // total))
+
--- 7week-CNN/model.py
+++ 7week-CNN/model.py
@@ -1,35 +1,39 @@
-from torch.nn import Module
-from torch import nn
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-class Model(Module):
+class LeNet5(nn.Module):
     def __init__(self):
-        super(Model, self).__init__()
+        super(LeNet5, self).__init__()
+        # 对应LeNet5中的C1层
+        # 输入通道为1，输出通道为6， 卷积核大小5x5，步长为1
         self.conv1 = nn.Conv2d(1, 6, 5)
-        self.relu1 = nn.ReLU()
-        self.pool1 = nn.MaxPool2d(2)
+        # 对应LeNet5中的S2层， 大小为2x2
+        self.pool1 = nn.MaxPool2d(2, 2)
+        # 对应LeNet5中的C3层
+        # 输入通道为6，输出通道为16，设置卷积核大小5x5，步长为1
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.relu2 = nn.ReLU()
-        self.pool2 = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(256, 120)
-        self.relu3 = nn.ReLU()
+        # 对应LeNet5中的S4层
+        self.pool2 = nn.MaxPool2d(2, 2)
+        # 对应LeNet5中的C5层， 输入通特征为4x4x16，输出特征为120x1
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        # 对应F6层，输入是120维向量，输出是84维向量
         self.fc2 = nn.Linear(120, 84)
-        self.relu4 = nn.ReLU()
+        # 输出层，输入是84维向量，输出是10维向量
         self.fc3 = nn.Linear(84, 10)
-        self.relu5 = nn.ReLU()
 
     def forward(self, x):
-        y = self.conv1(x)
-        y = self.relu1(y)
-        y = self.pool1(y)
-        y = self.conv2(y)
-        y = self.relu2(y)
-        y = self.pool2(y)
-        y = y.view(y.shape[0], -1)
-        y = self.fc1(y)
-        y = self.relu3(y)
-        y = self.fc2(y)
-        y = self.relu4(y)
-        y = self.fc3(y)
-        y = self.relu5(y)
-        return y
+        # [28,28,1]--->[24,24,6]--->[12,12,6]
+        x = self.pool1(F.relu(self.conv1(x)))
+        # [12,12,6]--->[8,8,,16]--->[4,4,16]
+        x = self.pool2(F.relu(self.conv2(x)))
+        # [n,4,4,16]--->[n,4*4*16]
+        x = x.view(-1, 16 * 4 * 4)
+        # [n,256]--->[n,120]
+        x = F.relu(self.fc1(x))
+        # [n,120]-->[n,84]
+        x = F.relu(self.fc2(x))
+        # [n,84]-->[n,10]
+        x = self.fc3(x)
+        return x
+
